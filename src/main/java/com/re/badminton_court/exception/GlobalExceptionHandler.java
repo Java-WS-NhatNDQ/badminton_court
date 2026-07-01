715cbe62dfdb6a62faed398cd4d4c41f5983bffc
Quang Nhat <ngduyquangnhat03@gmail.com>
2026-07-01 20:08:02 +0700

    up

--- src/main/java/com/re/badminton_court/exception/GlobalExceptionHandler.java
+++ src/main/java/com/re/badminton_court/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.re.badminton_court.exception;
 
 import com.re.badminton_court.model.dto.ApiResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
+
+import org.springframework.security.authentication.BadCredentialsException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -57,6 +60,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Void>> handleCloudStorageError(CloudStorageException ex) {
         return ResponseEntity.status(ex.getStatus())
                 .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                             .body("Sai tài khoản hoặc mật khẩu.");
     }
 
     @ExceptionHandler(Exception.class)
